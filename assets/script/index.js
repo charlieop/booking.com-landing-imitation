9c0946a27bad0a979fcb7e027f33c60b1c96084b
charlieop <spchanae@connect.ust.hk>
2025-01-03 16:45:28 +0800

    added date select

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,6 +3,112 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
   initDestinationsTabs();
   initScrollers();
+  initDateSelect();
+
+  document.addEventListener("click", (e) => {
+    const dateSelect = document.querySelector("#select-date");
+    if (!dateSelect.contains(e.target)) {
+      dateSelect.querySelector(".date-selector").style.display = "none";
+    } else {
+      dateSelect.querySelector(".date-selector").style.display = "block";
+    }
+  });
+}
+
+function initDateSelect() {
+  const dateSelect = document.querySelector("#select-date");
+  const monthYearDisplay = dateSelect.querySelector(".month-picker span");
+  const datePicker = dateSelect.querySelector(".date-picker");
+  const placeholders = datePicker.querySelectorAll(
+    ".date-picker .date .placeholder"
+  );
+  const dates = datePicker.querySelectorAll(".date-picker .date .date-item");
+  const dateDisplay = dateSelect.querySelector(".date-display");
+  let date = new Date();
+  let currentSelectedDate = new Date(date.getTime());
+
+  monthYearDisplay.textContent = date.toLocaleString("default", {
+    month: "short",
+    year: "numeric",
+  });
+  dates[currentSelectedDate.getDate() - 1].classList.add("selected");
+
+  const numPlaceholderToShow = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    1
+  ).getDay();
+  for (let i = 0; i < numPlaceholderToShow; i++) {
+    placeholders[i].classList.remove("hide");
+  }
+
+  const numDaysInMonth = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+  for (let i = numDaysInMonth - 1; i < 31; i++) {
+    dates[i].classList.add("hide");
+  }
+
+  // const input = dateSelect.querySelector("input");
+  // input.value = date.toISOString().split("T")[0];
+
+  dateSelect.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      if (e.target.classList.contains("prev")) {
+        date.setMonth(date.getMonth() - 1);
+      } else {
+        date.setMonth(date.getMonth() + 1);
+      }
+
+      monthYearDisplay.textContent = date.toLocaleString("default", {
+        month: "short",
+        year: "numeric",
+      });
+
+      const numPlaceholderToShow = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        1
+      ).getDay();
+      placeholders.forEach((placeholder) => placeholder.classList.add("hide"));
+      dates.forEach((date) => date.classList.remove("hide"));
+      for (let i = 0; i < numPlaceholderToShow; i++) {
+        placeholders[i].classList.remove("hide");
+      }
+      const numDaysInMonth = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+      ).getDate();
+      for (let i = numDaysInMonth; i < 31; i++) {
+        dates[i].classList.add("hide");
+      }
+
+      if (
+        date.getMonth() === currentSelectedDate.getMonth() &&
+        date.getFullYear() === currentSelectedDate.getFullYear()
+      ) {
+        dates[currentSelectedDate.getDate() - 1].classList.add("selected");
+      } else {
+        dates[currentSelectedDate.getDate() - 1].classList.remove("selected");
+      }
+    } else if (e.target.classList.contains("date-item")) {
+      currentSelectedDate = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        e.target.textContent
+      );
+      dates.forEach((date) => date.classList.remove("selected"));
+      e.target.classList.add("selected");
+      dateDisplay.textContent = currentSelectedDate.toLocaleString("default", {
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+      });
+    }
+  });
 }
 
 function initScrollers() {
